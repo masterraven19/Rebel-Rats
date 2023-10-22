7077b5b6debf0901b612e6c46201737e0ed331ba
masterraven19 <139358930+masterraven19@users.noreply.github.com>
2023-10-22 14:35:00 +0800

    added extra fx to the spinaldoldrums

--- src/data/scripts/weapons/rebelrats_spinalDoldrumsEffect.java
+++ src/data/scripts/weapons/rebelrats_spinalDoldrumsEffect.java
@@ -11,7 +11,8 @@ import org.lwjgl.util.vector.Vector2f;
 import java.awt.*;
 
 public class rebelrats_spinalDoldrumsEffect implements EveryFrameWeaponEffectPlugin {
-    final Vector2f endSize = new Vector2f(40,40);
+    final Vector2f endSize = new Vector2f(30,30);
+    final Vector2f endSize2 = new Vector2f(50,50);
     private float elapsed = 0;
     private float scale = 0;
     private float angle = 0;
@@ -24,14 +25,14 @@ public class rebelrats_spinalDoldrumsEffect implements EveryFrameWeaponEffectPlu
         float chargetime = weapon.getOriginalSpec().getChargeTime();
         Vector2f loc = rebelrats_combatUtils.calcLocWAngle(weapon.getCurrAngle(), 50, weapon.getLocation());
 
-        angle += 1;
+        angle += 2;
         if (angle > 360) {angle = 0;}
         
         if (elapsed < chargetime * 0.7 && scale < 1){
             scale += amount;
         }
         if (elapsed > chargetime * 0.7 && scale > 0){
-            scale -= amount;
+            scale -= 0.1f;
         }
         if (elapsed > chargetime){
             elapsed -= chargetime;
@@ -39,9 +40,16 @@ public class rebelrats_spinalDoldrumsEffect implements EveryFrameWeaponEffectPlu
         }
 
         Vector2f size = new Vector2f(endSize.x * scale, endSize.y * scale);
+        Vector2f size2 = new Vector2f(endSize2.x * scale, endSize2.y * scale);
+
         rebelrats_addExplosionFx p = new rebelrats_addExplosionFx();
-        p.addExplosion("graphics/fx/explosion0.png",null,size,size,loc,weapon.getShip().getVelocity(),new Color(67,67,222,255),0.1f,0.1f,0,0,angle,1,false);
+        p.addExplosion("graphics/fx/fog_circle.png",null,size,size,loc,weapon.getShip().getVelocity(),new Color(67,67,222,255),0.05f,0.1f,0,0,angle,1,false);
         CombatEntityAPI e = engine.addLayeredRenderingPlugin(p);
         e.getLocation().set(weapon.getLocation());
+
+        rebelrats_addExplosionFx p2 = new rebelrats_addExplosionFx();
+        p2.addExplosion("graphics/fx/explosion3.png",null,size2,size2,loc,weapon.getShip().getVelocity(),new Color(10,10,208,255),0.05f,0.1f,0,0,angle,1f,false);
+        CombatEntityAPI e2 = engine.addLayeredRenderingPlugin(p2);
+        e2.getLocation().set(weapon.getLocation());
     }
 }
