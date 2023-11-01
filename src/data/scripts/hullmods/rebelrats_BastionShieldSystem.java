39495e7c1b41bcb5ec9c3e28668d0389c757f715
masterraven19 <139358930+masterraven19@users.noreply.github.com>
2023-11-02 01:44:10 +0800

    Added S-Mod bonus to bastion shield
    Also made hardened shield bonus to the bastion shield system 0.1 instead of 0.2

--- src/data/scripts/hullmods/rebelrats_BastionShieldSystem.java
+++ src/data/scripts/hullmods/rebelrats_BastionShieldSystem.java
@@ -9,16 +9,17 @@ import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 import java.awt.*;
 
 public class rebelrats_BastionShieldSystem extends BaseHullMod {
-    private static float shieldRadiusOffset = -0.1F;
-    private static float shieldArcMax = 150;
-    private static float shieldDebuff = 0.2F;
-    private static float elapsed = 0;
-    private static String modId = null;
+    private final float shieldRadiusOffset = -0.1F;
+    private final float shieldArcMax = 150;
+    private final float shieldDebuff = 0.2F;
+    private final float sModShieldDebuff = 0F;
+    private final float hardenedShieldBuff = 0.1F;
+    private String modId = null;
     public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
-
+        modId = id;
     }
     public void applyEffectsAfterShipCreation(ShipAPI ship, String id) {
-        modId = id;
+
     }
     public void advanceInCombat(ShipAPI ship, float amount) {
         if (Global.getCombatEngine().isPaused()) return;
@@ -72,101 +73,36 @@ public class rebelrats_BastionShieldSystem extends BaseHullMod {
 
             efficiencyRatio = diffRatio * shieldDebuff;
         }
-        /**
-        float angleDifference = frontAngle - shieldAngle;
-        if (angleDifference >= 0 && angleDifference <= 90){
-            float diffRatio = angleDifference * 0.01F;
-            if (diffRatio == 0.9){diffRatio = 1;}
-
-            targetArc = (diffRatio * arcDifference) + shieldArc;
-
-            shieldRatio = diffRatio * shieldRadiusOffset;
-
-            efficiencyRatio = diffRatio * shieldDebuff;
-        }
-        if (angleDifference >= 90 && angleDifference <= 180){
-            float diffRatio = 1 - ((angleDifference - 90) * 0.01F);
-            if (diffRatio == 0.1){diffRatio = 0;}
-
-            targetArc = (diffRatio * arcDifference) + shieldArc;
-
-            shieldRatio = diffRatio * shieldRadiusOffset;
-
-            efficiencyRatio = diffRatio * shieldDebuff;
-        }
-        if (angleDifference >= 180 && angleDifference <= 270){
-            float diffRatio = (angleDifference - 180) * 0.01F;
-            if (diffRatio == 0.9){diffRatio = 1;}
-
-            targetArc = (diffRatio * arcDifference) + shieldArc;
-
-            shieldRatio = diffRatio * shieldRadiusOffset;;
-
-            efficiencyRatio = diffRatio * shieldDebuff;
-        }
-        if (angleDifference >= 270 && angleDifference <= 360){
-            float diffRatio = 1 - ((angleDifference - 270) * 0.01F);
-            if (diffRatio == 0.1){diffRatio = 0;}
-
-            targetArc = (diffRatio * arcDifference) + shieldArc;
-
-            shieldRatio = diffRatio * shieldRadiusOffset;
-
-            efficiencyRatio = diffRatio * shieldDebuff;
-        }
-        if (angleDifference <= 0 && angleDifference >= -90){
-            float diffRatio = angleDifference * -0.01F;
-            if (diffRatio == 0.9){diffRatio = 1;}
-
-            targetArc = (diffRatio * arcDifference) + shieldArc;
-
-            shieldRatio = diffRatio * shieldRadiusOffset;
-
-            efficiencyRatio = diffRatio * shieldDebuff;
-        }
-        if (angleDifference <= -90 && angleDifference >= -180){
-            float diffRatio = 1 - ((angleDifference + 90) * -0.01F);
-            if (diffRatio == 0.1){diffRatio = 0;}
-
-            targetArc = (diffRatio * arcDifference) + shieldArc;
-
-            shieldRatio = diffRatio * shieldRadiusOffset;
-
-            efficiencyRatio = diffRatio * shieldDebuff;
-        }
-        if(angleDifference <= -180 && angleDifference >= -270){
-            float diffRatio = (angleDifference + 180) * -0.01F;
-            if (diffRatio == 0.9){diffRatio = 1;}
-
-            targetArc = (diffRatio * arcDifference) + shieldArc;
-
-            shieldRatio = diffRatio * shieldRadiusOffset;
-
-            efficiencyRatio = diffRatio * shieldDebuff;
-        }
-        if(angleDifference <= -270 && angleDifference >= -360){
-            float diffRatio = 1 - ((angleDifference + 270) * -0.01F);
-            if (diffRatio == 0.1){diffRatio = 0;}
-
-            targetArc = (diffRatio * arcDifference) + shieldArc;
-
-            shieldRatio = diffRatio * shieldRadiusOffset;
-
-            efficiencyRatio = diffRatio * shieldDebuff;
-        }
-        **/
 
         float shieldRadiusTarget = (1 + shieldRatio) * shieldRadius;
+        float efficiency = efficiencyRatio;
 
-        ship.getMutableStats().getShieldDamageTakenMult().modifyMult(modId, 1 + efficiencyRatio);
+        boolean sMod = isSMod(ship);
+        if (sMod) {
+            efficiency = sModShieldDebuff;
+        }else{
+            for(String mod: ship.getVariant().getHullMods()){
+                if (mod.equals(modId)) continue;
+                if (mod.equals("hardenedshieldemitter")){
+                    if (efficiency > hardenedShieldBuff){
+                        efficiency = efficiency - hardenedShieldBuff;
+                    }
+                }
+            }
+        }
+
+        ship.getMutableStats().getShieldDamageTakenMult().modifyMult(modId, 1 + efficiency);
         ship.getShield().setRadius(shieldRadiusTarget, "graphics/fx/ratshieldouter.png", "graphics/fx/ratshieldouterring.png");
         ship.getShield().setArc(targetArc);
-
     }
 
     public String getDescriptionParam(int index, HullSize hullSize) {
         if (index == 0) return "" + (int) shieldArcMax;
         if (index == 1) return "" + shieldDebuff;
+        return null;
+    }
+    public String getSModDescriptionParam(int index, HullSize hullSize) {
+        if (index == 0) return "" + 0;
         return null;
     }
 }
