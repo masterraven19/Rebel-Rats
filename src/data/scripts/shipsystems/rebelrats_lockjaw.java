8ffee9fca8d95f8852214f5dab1212f67dc3a9da
masterraven19 <139358930+masterraven19@users.noreply.github.com>
2024-01-21 19:19:00 +0800

    Changed stats of Lockjaw system
    Ballistic dmg bonus 60->50%
    Ballistic RoF debuff to bonus
    -30->30%

--- src/data/scripts/shipsystems/rebelrats_lockjaw.java
+++ src/data/scripts/shipsystems/rebelrats_lockjaw.java
@@ -4,11 +4,11 @@ import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
 
 public class rebelrats_lockjaw extends BaseShipSystemScript {
-    public static final float ballisticDamageBonus = 60;
-    public static final float ballisticROFDebuff = -30;
+    public static final float ballisticDamageBonus = 50;
+    public static final float ballisticROFBuff = 30;
     public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
         float ballisticBonus = ballisticDamageBonus * effectLevel;
-        float ballisticDebuff = ballisticROFDebuff * effectLevel;
+        float ballisticDebuff = ballisticROFBuff * effectLevel;
         stats.getBallisticWeaponDamageMult().modifyPercent(id,ballisticBonus);
         stats.getBallisticRoFMult().modifyPercent(id,ballisticDebuff);
     }
@@ -19,12 +19,12 @@ public class rebelrats_lockjaw extends BaseShipSystemScript {
 
     public StatusData getStatusData(int index, State state, float effectLevel) {
         float bonusPercent = ballisticDamageBonus * effectLevel;
-        float rofDebuff = ballisticROFDebuff * effectLevel;
+        float rofDebuff = ballisticROFBuff * effectLevel;
         if (index == 0) {
             return new StatusData("+" + (int) bonusPercent + "% ballistic weapon damage" , false);
         }
         if (index == 1){
-            return new StatusData("-" + (int) rofDebuff + "% ballistic RoF" , true);
+            return new StatusData("+" + (int) rofDebuff + "% ballistic RoF" , false);
         }
         return null;
     }
