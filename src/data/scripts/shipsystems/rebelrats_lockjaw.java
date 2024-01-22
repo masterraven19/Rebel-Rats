cdb7f28b5c1d0975e4d991dbe4a747baffd89765
masterraven19 <139358930+masterraven19@users.noreply.github.com>
2024-01-22 19:37:29 +0800

    Changed lockjaw
    ROF buff changed from ROF to range buff

--- src/data/scripts/shipsystems/rebelrats_lockjaw.java
+++ src/data/scripts/shipsystems/rebelrats_lockjaw.java
@@ -1,25 +1,27 @@
 package data.scripts.shipsystems;
 
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.WeaponAPI;
 import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
 
 public class rebelrats_lockjaw extends BaseShipSystemScript {
-    public static final float ballisticDamageBonus = 50;
-    public static final float ballisticROFBuff = 30;
+    private static final float ballisticDamageBonus = 50;
+    private static final float ballisticRangeBuff = 30;
     public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
         float ballisticBonus = ballisticDamageBonus * effectLevel;
-        float ballisticDebuff = ballisticROFBuff * effectLevel;
+        float ballisticDebuff = ballisticRangeBuff * effectLevel;
         stats.getBallisticWeaponDamageMult().modifyPercent(id,ballisticBonus);
-        stats.getBallisticRoFMult().modifyPercent(id,ballisticDebuff);
+        stats.getBallisticWeaponRangeBonus().modifyPercent(id,ballisticDebuff);
     }
     public void unapply(MutableShipStatsAPI stats, String id) {
         stats.getBallisticWeaponDamageMult().unmodify(id);
-        stats.getBallisticRoFMult().unmodify(id);
+        stats.getBallisticWeaponRangeBonus().unmodify(id);
     }
 
     public StatusData getStatusData(int index, State state, float effectLevel) {
         float bonusPercent = ballisticDamageBonus * effectLevel;
-        float rofDebuff = ballisticROFBuff * effectLevel;
+        float rofDebuff = ballisticRangeBuff * effectLevel;
         if (index == 0) {
             return new StatusData("+" + (int) bonusPercent + "% ballistic weapon damage" , false);
         }
