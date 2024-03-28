a4c9703589b7a47ce272b274efddafe5631c17ae
masterraven19 <139358930+masterraven19@users.noreply.github.com>
2024-03-29 00:42:45 +0800

    Tinkering and cleaning

--- src/data/scripts/world/systems/rebelrats_dragonsblessing/gen_NPCs.java
+++ src/data/scripts/world/systems/rebelrats_dragonsblessing/gen_NPCs.java
@@ -7,25 +7,20 @@ import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.characters.FullName;
 import com.fs.starfarer.api.characters.PersonAPI;
 
-public class gen_NPCs {//fix later, plan is to make class that returns a given person
-    public static PersonAPI Administrator_Example(MarketAPI market){
+public class gen_NPCs {
+    public static void Administrator_Example(MarketAPI market){//doesnt appear
         PersonAPI p = Global.getFactory().createPerson();
-        FullName name = new FullName("Nazrin","Naides", FullName.Gender.FEMALE);
+        FullName name = new FullName("Nazarin","Dess", FullName.Gender.FEMALE);
         p.setName(name);
+        p.setId("rebelrats_nazarin");
         p.setFaction("rebelrats");
         p.setImportance(PersonImportance.VERY_HIGH);
         p.setRankId("Rat Commander");
+        market.getCommDirectory().addPerson(p,1);
+        market.setAdmin(p);
         p.setMarket(market);
-        p.getMarket().setAdmin(p);
-        return p;
     }
     public static void generateNPCS(PlanetAPI planet, MarketAPI market){
-        switch(planet.getName()){
-            case "Krysa":
-                Administrator_Example(market);
-                break;
-            default:
-                break;
-        }
+        Administrator_Example(market);
     }
 }
