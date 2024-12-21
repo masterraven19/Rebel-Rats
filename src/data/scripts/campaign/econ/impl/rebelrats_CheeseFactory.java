80565dd05bf32288a3860dc924f8313270ea2b3d
masterraven19 <139358930+masterraven19@users.noreply.github.com>
2024-12-21 22:41:59 +0800

    Added CHEEESE

---
+++ src/data/scripts/campaign/econ/impl/rebelrats_CheeseFactory.java
@@ -0,0 +1,33 @@
+package data.scripts.campaign.econ.impl;
+
+import com.fs.starfarer.api.impl.campaign.econ.impl.BaseIndustry;
+import com.fs.starfarer.api.impl.campaign.ids.Commodities;
+import com.fs.starfarer.api.util.Pair;
+
+public class rebelrats_CheeseFactory extends BaseIndustry {
+
+    @Override
+    public void apply() {
+        super.apply(true);
+        int size = market.getSize();
+
+        demand(Commodities.FOOD,size);
+        supply("rebelrats_cheese",size);
+
+        Pair<String,Integer> deficit = getMaxDeficit(Commodities.FOOD);
+        applyDeficitToProduction(1, deficit,
+                "rebelrats_cheese");
+
+        if (!isFunctional()) {
+            supply.clear();
+        }
+    }
+    @Override
+    public void unapply() {
+        super.unapply();
+    }
+    @Override
+    protected boolean canImproveToIncreaseProduction() {
+        return true;
+    }
+}
