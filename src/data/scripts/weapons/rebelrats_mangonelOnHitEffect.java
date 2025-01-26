50e685d6070a2180863deb7be9202ce38348e04b
masterraven19 <139358930+masterraven19@users.noreply.github.com>
2025-01-26 22:30:15 +0800

    mangonel shrapnel
    reflects

--- src/data/scripts/weapons/rebelrats_mangonelOnHitEffect.java
+++ src/data/scripts/weapons/rebelrats_mangonelOnHitEffect.java
@@ -5,6 +5,7 @@ import com.fs.starfarer.api.combat.listeners.ApplyDamageResultAPI;
 import data.scripts.combat.rebelrats_addExplosionFx;
 import data.scripts.combat.rebelrats_addParticle;
 import data.scripts.combat.rebelrats_combatUtils;
+import org.lazywizard.lazylib.MathUtils;
 import org.lwjgl.util.vector.Vector2f;
 
 import java.awt.*;
@@ -14,24 +15,44 @@ public class rebelrats_mangonelOnHitEffect implements OnHitEffectPlugin {
     public void onHit(DamagingProjectileAPI projectile, CombatEntityAPI target, Vector2f point, boolean shieldHit, ApplyDamageResultAPI damageResult, CombatEngineAPI engine) {
         if(!(target instanceof ShipAPI)) return;
         ShipAPI ship = (ShipAPI) target;
+        float ratioCone = 60;
         float hullDmg = 300f;
-        DamageType hullDmgType = DamageType.FRAGMENTATION;
         float shieldDmg = 100f;
+        DamageType hullDmgType = DamageType.FRAGMENTATION;
         DamageType shieldDmgType = DamageType.KINETIC;
 
         if(shieldHit){
             engine.applyDamage(ship,point,shieldDmg,shieldDmgType,0,false,false,projectile.getSource());
 
             float scale = (ship.getCurrFlux()/ship.getMaxFlux()) * 1;
-            int amt = (int)(scale * 15);
+            int shrapamt = (int)(scale * 15);
 
-            for(int i = 0;i < amt;i++){
-                float angle = rebelrats_combatUtils.calcConeAngle(rebelrats_combatUtils.randomNumber(120,170),projectile.getFacing() - 180);
-                Vector2f loc = rebelrats_combatUtils.calcLocWAngle(projectile.getFacing() - 180, 50, point);
+            for(int i = 0;i < shrapamt;i++){
+                float pFacing = projectile.getFacing();
+                float reflectAngle;
 
-                DamagingProjectileAPI p = (DamagingProjectileAPI) engine.spawnProjectile(projectile.getSource(), null, "rebelrats_mangonel_shrapnel", loc, projectile.getFacing(), projectile.getSource().getVelocity());
-//                Vector2f speed = rebelrats_combatUtils.calcVelDir(angle,200);
-//                p.getVelocity().set(speed); try this tmrw
+                float normal = rebelrats_combatUtils.calcDirectionOfTwoPoints(point,target.getLocation());
+                if(pFacing > 0){reflectAngle = pFacing -180;}
+                else{reflectAngle = pFacing + 180;}
+                float diff = normal - reflectAngle;
+                float reflectDiff = diff;
+                if(diff < -180){reflectDiff += 360;}
+                if(diff > 180){reflectDiff -= 360;}
+                if(reflectDiff < 0){reflectDiff *= -1;}
+                if(reflectDiff > ratioCone){reflectDiff = ratioCone;}
+                float ratio = reflectDiff / ratioCone;
+                reflectAngle = normal + (ratio * diff);
+
+                float coneAngle = 170 - (155 * ratio);
+                float angle = rebelrats_combatUtils.calcConeAngle(coneAngle,reflectAngle);
+                float speed = rebelrats_combatUtils.randomNumber(1000,1500);
+                float fadout = (((speed - 999)/500) * 0.3f) + 0.08f;
+                Vector2f velocity = rebelrats_combatUtils.calcVelDir(angle,speed);
+
+                rebelrats_addParticle p = new rebelrats_addParticle();
+                p.addParticle(null,"fx","rebelrats_mangonel_plasma",80,20,point,new Vector2f(0,0),velocity,angle,0,false,0,0.3f,1f,fadout,false,null);
+                CombatEntityAPI e = engine.addLayeredRenderingPlugin(p);
+                e.getLocation().set(point);
             }
         }else{
             engine.applyDamage(ship,point,hullDmg,hullDmgType,0,false,false,projectile.getSource());
@@ -42,21 +63,41 @@ public class rebelrats_mangonelOnHitEffect implements OnHitEffectPlugin {
             float armot = ship.getArmorGrid().getArmorValue(cell[0],cell[1]);
             float max = ship.getArmorGrid().getMaxArmorInCell();
             float scale = (armot/max) * 1;
-            int amt = (int)(scale * 15);
+            int shrapamt = (int)(scale * 15);
+            int cloudamt = 3;
 
             if(armot > 0){
-                for(int i = 0;i < amt;i++){
-                    float angle = rebelrats_combatUtils.calcConeAngle(rebelrats_combatUtils.randomNumber(120,170),projectile.getFacing() - 180);
-                    Vector2f loc = rebelrats_combatUtils.calcLocWAngle(projectile.getFacing() - 180, 50, point);
+                for(int i = 0;i < shrapamt;i++){
+                    float pFacing = projectile.getFacing();
+                    float reflectAngle;
 
-                    CombatEntityAPI p = engine.spawnProjectile(projectile.getSource(), null, "rebelrats_mangonel_shrapnel", loc, projectile.getFacing(), projectile.getSource().getVelocity());
-                    p.setFacing(angle);
-                    p.getVelocity().scale((float)Math.random());
+                    float normal = rebelrats_combatUtils.calcDirectionOfTwoPoints(point,target.getLocation());
+                    if(pFacing > 0){reflectAngle = pFacing -180;}
+                    else{reflectAngle = pFacing + 180;}
+                    float diff = normal - reflectAngle;
+                    float reflectDiff = diff;
+                    if(diff < -180){reflectDiff += 360;}
+                    if(diff > 180){reflectDiff -= 360;}
+                    if(reflectDiff < 0){reflectDiff *= -1;}
+                    if(reflectDiff > ratioCone){reflectDiff = ratioCone;}
+                    float ratio = reflectDiff / ratioCone;
+                    reflectAngle = normal + (ratio * diff);
+
+                    float coneAngle = 170 - (155 * ratio);
+                    float angle = rebelrats_combatUtils.calcConeAngle(coneAngle,reflectAngle);
+                    float speed = rebelrats_combatUtils.randomNumber(1000,1500);
+                    float fadout = (((speed - 999)/500) * 0.3f) + 0.08f;
+                    Vector2f velocity = rebelrats_combatUtils.calcVelDir(angle,speed);
+
+                    rebelrats_addParticle p = new rebelrats_addParticle();
+                    p.addParticle(null,"fx","rebelrats_mangonel_plasma",80,20,point,new Vector2f(0,0),velocity,angle,0,false,0,0.3f,1f,fadout,false,null);
+                    CombatEntityAPI e = engine.addLayeredRenderingPlugin(p);
+                    e.getLocation().set(point);
                 }
             }else{
-                for(int i = 0;i < 3;i++){
+                for(int i = 0;i < cloudamt;i++){
                     rebelrats_addExplosionFx p = new rebelrats_addExplosionFx();
-                    p.addExplosion("misc","nebula_particles",new Vector2f(50,50),new Vector2f(80,80),point,new Vector2f(0,0),new Color(244,0,0),3.5F,4,1,0, rebelrats_combatUtils.randomNumber(0,360),0.7F,true);
+                    p.addExplosion("misc","nebula_particles",new Vector2f(50,50),new Vector2f(80,80),point,new Vector2f(0,0),new Color(244,0,0),1.5F,2,1,0, rebelrats_combatUtils.randomNumber(0,360),0.7F,true);
                     CombatEntityAPI e = engine.addLayeredRenderingPlugin(p);
                     e.getLocation().set(point);
                 }
