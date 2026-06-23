a0eb0c4ccd0b79f58a9c6c041aa7901506a19fb0
masterraven19 <139358930+masterraven19@users.noreply.github.com>
2026-06-23 13:05:36 +0800

    Update rebelrats_Markets.java

--- src/data/scripts/campaign/ids/rebelrats_Markets.java
+++ src/data/scripts/campaign/ids/rebelrats_Markets.java
@@ -2,4 +2,11 @@ package data.scripts.campaign.ids;
 
 public class rebelrats_Markets {
     public static final String krysa = "rebelrats_rattus_market";
+    public static final String malus = "rebelrats_malus_market";
+    public static final String nutria = "rebelrats_nutria_market";
+    public static final String rodentia = "rebelrats_rodentia_market";
+    public static final String outerStation = "rebelrats_outerstation_market";
+    public static final String tikus = "rebelrats_tikus_market";
+    public static final String tikusII = "rebelrats_tikusII_market";
+    public static final String tikusIII = "rebelrats_tikusIII_market";
 }
